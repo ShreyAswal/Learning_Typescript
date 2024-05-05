ceb667de39af84d605aa8c7c4209939720c6055e
ShreyAswal <aswalshrey@gmail.com>
2024-05-05 19:41:50 +0530

    Setup config settings

--- index.ts
+++
@@ -1,2 +0,0 @@
-// Let adn const get converted to var in the compiled js file
-var a: number = 20;
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+// Let and const get converted to var in the compiled js file
+var a: number = 20;
