0e0c2f36e7d9e0badd8210f40d4a5a118eecb083
ShreyAswal <aswalshrey@gmail.com>
2024-05-07 22:50:30 +0530

    Built in types & annotation

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
-let age: number = 20;
-if(age < 50)
-    age += 10;
-console.log(age);
+// First typescript program
+// let age: number = 20;
+
+// Debugging logic in typescript
+// if(age < 50)
+//     age += 10;
+// console.log(age);
+
+// Built in data types
+// If we don't specify the type, it will be any
+// If we annotate the type and then remove the annotation, it will be of the same type
+let sales = 123_456_789; // First annotated to number
+let course: string = "TypeScript";
+let isPublished = true; // First annotated to boolean
+let level;
+
